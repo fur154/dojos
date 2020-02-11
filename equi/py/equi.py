01d646c197f7cb90ec22d8f5dd50bc12f3a2884e
fur154 <60931417+fur154@users.noreply.github.com>
2020-02-11 16:51:39 +0200

    Update equi.py

--- equi/py/equi.py
+++ equi/py/equi.py
@@ -1,15 +1,28 @@
 import unittest
 
 def find_equi_index(inArray, numElements):
-	# Find the equilibrium index of input array
-	return -1
+    name = inArray
+    equi = []
+    i=0
+    for index in range(len(name)):
+        if index == 0:
+            if sum(name[index+1:]) == 0:
+                equi = index
+        elif index == len(name)-1:
+            if sum(name[0:index]) == 0:
+                equi = index
+        else :
+            if sum(name[0:index]) == sum(name[index+1:]):
+                equi = index
+    if equi == []:
+        return -1
+    if equi != []:
+        return equi
 
-class TestEquiIndex(unittest.TestCase):
-	def test_signelNumber(self):
-		inArray = [3]
-		expectedP = 0
-		P = find_equi_index(inArray, len(inArray))
-		self.assertEqual(expectedP, P)
+
+A = [-5,-2,3,-1,-6] 
+answ=find_equi_index(A,3)
+print(answ)
 
 
 if __name__ == '__main__':
